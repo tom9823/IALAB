24c997b165bfc881fec1844a14fc580fcf28efc3
LUCA TOMATIS <luca.tomatis1@esternibisp.com>
2025-08-11 22:59:11 +0200

    upd

--- Prolog/Labirinto/ida_star.pl
+++ Prolog/Labirinto/ida_star.pl
@@ -1,27 +1,97 @@
-initialize:-
+initialize :-
     retractall(soglia(_)),
-    asserta(soglia(1)).
-itdeep(Cammino):-
-    soglia(S),
-    profondita(Cammino,S).
-itdeep(Cammino):-
-    soglia(S),
-    SNuova is S+1,
-    retractall(soglia(_)),
-    asserta(soglia(SNuova)),
-    itdeep(Cammino).
-
-profondita(Cammino,Soglia):-
+    retractall(salvati(_)),
     iniziale(S0),
-    ricerca(S0,[S0],Soglia,Cammino).
-ricerca(S,_,_,[]):-finale(S).
-ricerca(S,Visitati,MaxDepth,[Azione|Cammino]):-
-    MaxDepth > 0,
-    applicabile(Azione,S),
-    trasforma(Azione,S,SNuovo),
-    \+member(SNuovo,Visitati),
-    NewMax is MaxDepth-1,
-    ricerca(SNuovo,[SNuovo|Visitati],NewMax,Cammino).
+    h(S0, H0),
+    asserta(soglia(H0)),
+    asserta(salvati([])).
 
+% 1) prova entro la soglia corrente (bound su f)
+ida_star(Cammino) :-
+    soglia(Bound),
+    retractall(salvati(_)), asserta(salvati([])),   % azzera i tagli dell’iterazione
+    profondita(Cammino, Bound).                     % se trova, questa clausola ha successo
 
-h(pos(Xi,Yi),pos(Xf,Yf), DeltaX, DeltaY):- iniziale(pos(Xi,Yi)), finale(pos(Xf,Yf)), DeltaX is abs(Xi-Xf), DeltaY is abs(Yi-Yf).
+% 2) se non ha trovato: nuova soglia = min degli f(n) tagliati
+ida_star(Cammino) :-
+    salvati(L), L \= [],                            % evita min su lista vuota
+    min(L, NextBound),
+    retractall(soglia(_)),
+    asserta(soglia(NextBound)),
+    ida_star(Cammino).
+
+profondita(Cammino, Bound) :-
+    iniziale(S0),
+    ricerca(S0, 0, [S0], Bound, Cammino).
+
+% --- RICERCA DFS con bound su f = g + h ---
+
+% Successo: goal con f <= Bound
+ricerca(S, G, _Visitati, Bound, []) :-
+    f(G, S, F),
+    F =< Bound,
+    finale(S), !.
+
+% Taglio: f > Bound -> salva f(n) e fallisci per backtracking
+ricerca(S, G, _Visitati, Bound, _Cammino) :-
+    f(G, S, F),
+    F > Bound, !,
+    salvati(L0),
+    retract(salvati(L0)),
+    asserta(salvati([F|L0])),
+    fail.
+
+% Espansione normale (costi unitari: G1 = G+1)
+ricerca(S, G, Visitati, Bound, [Azione|Cammino]) :-
+    applicabile(Azione, S),
+    trasforma(Azione, S, S1),
+    \+ member(S1, Visitati),
+    G1 is G + 1,
+    ricerca(S1, G1, [S1|Visitati], Bound, Cammino).
+
+% f(G,S) = g + h
+f(G, S, F) :- h(S, H), F is G + H.
+
+distanza_manhattan(pos(Xi,Yi), pos(Xf,Yf), Ris) :- DeltaX is abs(Xi-Xf), DeltaY is abs(Yi-Yf), Ris is DeltaX + DeltaY.
+
+% conta quante finali/1 sono definite
+conta_finali(N) :-
+    conta_finali_acc([], 0, N).
+
+conta_finali_acc(Visti, Acc, N) :-
+    finale(F),
+    \+ member(F, Visti), !,
+    Acc1 is Acc + 1,
+    conta_finali_acc([F|Visti], Acc1, N).
+conta_finali_acc(_, Acc, Acc).
+
+% euristica h(S,H): minima Manhattan verso la finale più vicina
+h(S, 0) :-
+    finale(S), !.
+
+h(S, H) :-
+    conta_finali(N), N > 0,
+    mia_h(S, [], N, _, H), !.
+
+% mia_h(S, Visti, N, Best0, Best):
+%   visita N finali non ancora viste, aggiornando il migliore (minimo)
+mia_h(_, _, 0, Best, Best) :- nonvar(Best), !.
+mia_h(S, Visti, N, Best0, Best) :-
+    finale(F),
+    \+ member(F, Visti), !,
+    distanza_manhattan(S, F, D),
+    update_best(Best0, D, Best1),
+    N1 is N - 1,
+    mia_h(S, [F|Visti], N1, Best1, Best).
+
+% aggiorna il minimo corrente
+update_best(B0, D, D) :- var(B0), !.
+update_best(B0, D, D) :- number(B0), D < B0, !.
+update_best(B0, _, B0).
+
+min([], _):- !,false.
+min([Head | Tail], Ris) :- Tail = [], !, Ris is Head.
+min([Head | Tail],Ris) :- min_acc(Tail,Head, Ris).
+min_acc([Head | Tail], Min, Ris) :- Min > Head, !, min_acc(Tail, Head, Ris).
+min_acc([Head | Tail], Min, Ris) :- Min =< Head, !, min_acc(Tail, Min, Ris).
+min_acc([], Min, Min).
